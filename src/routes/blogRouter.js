bac394d55921b19a3dc6cac45c4aadffd1b69c4d
VishishtaDilsara <rgvdilsara2002@gmail.com>
2026-02-07 15:07:52 +0530

    Blog Router created

---
+++ src/routes/blogRouter.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import {
+  createBlogHandler,
+  deleteBlogHandler,
+  getBlogByIdHandler,
+  getBlogsHandler,
+  updateBlogHandler,
+} from "../controllers/blogController";
+import { requireOwnerOrAdmin } from "../middlewares/ownershipMiddleware";
+
+const blogRouter = Router();
+
+// POST /blogs (Protected)
+blogRouter.post("/", authMiddleware, createBlogHandler);
+
+// GET /blogs (Pagination)
+blogRouter.get("/", getBlogsHandler);
+
+// GET /blogs/:id
+blogRouter.get("/:id", getBlogByIdHandler);
+
+// PUT /blogs/:id (Owner or Admin only)
+blogRouter.put("/:id", authMiddleware, requireOwnerOrAdmin, updateBlogHandler);
+
+// DELETE /blogs/:id (Admin only)
+blogRouter.delete(
+  "/:id",
+  authMiddleware,
+  requireRole("ADMIN"),
+  deleteBlogHandler,
+);
+
+export default blogRouter;
